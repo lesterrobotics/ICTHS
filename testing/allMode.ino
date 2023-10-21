ac813c2e3123e1de16b385484644be63a6a4d95a
lesterrobotics <134779190+lesterrobotics@users.noreply.github.com>
2023-10-21 15:10:35 +0800

    Update and rename shiftOut.ino to allMode.ino

---
+++ testing/allMode.ino
@@ -0,0 +1,28 @@
+void allMode(uint8_t firstPin, uint8_t lastPin, byte IO)
+{
+    amtOfPins = lastPin - firstPin + 1
+    for (uint8_t counter = firstPin; counter <= lastPin; counter++)
+    {
+        pinMode(counter, IO);
+        Serial.print("Pin ");
+        Serial.print(counter);
+        Serial.print(" set as ");
+        if ( IO == INPUT )
+        {
+            Serial.println("INPUT");
+        } else {
+            Serial.println("OUTPUT");
+        }
+    }
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    allMode(2, 13, OUTPUT);
+}
+
+void loop()
+{
+    
+}
