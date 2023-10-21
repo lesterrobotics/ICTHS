bd606ea3209f3bf36714d09e2b8f4c49438937d7
lesterrobotics <134779190+lesterrobotics@users.noreply.github.com>
2023-10-22 00:10:06 +0800

    Create manual_shifting.ino

---
+++ testing/manual_shifting.ino
@@ -0,0 +1,35 @@
+#define DATA_PIN 11
+#define SHIFT_PIN 13
+#define LATCH_PIN 12
+
+void allMode( uint8_t firstPin, uint8_t lastPin, byte IO )
+{
+    uint8_t amtOfPins = lastPin - firstPin + 1;
+    for ( uint8_t counter = firstPin; counter <= lastPin; counter++ )
+    {
+        pinMode( counter, IO );
+        Serial.print( "Pin " );
+        Serial.print( counter );
+        Serial.print( " set as " );
+        if ( IO == INPUT )
+        {
+            Serial.println( "INPUT" );
+        } else {
+            Serial.println( "OUTPUT" );
+        }
+    }
+}
+
+void shiftWrite( unsigned long data ) {
+    
+}
+
+void setup()
+{
+    Serial.begin( 9600 );
+    allMode( 2, 13, OUTPUT );
+}
+
+void loop()
+{
+}
