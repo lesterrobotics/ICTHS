d18111f2fbc1cf141e5eb8142ac12b664234964d
lesterrobotics <134779190+lesterrobotics@users.noreply.github.com>
2023-10-31 19:09:29 +0800

    Update abdulbukul.ino

--- testing/abdulbukul.ino
+++ testing/abdulbukul.ino
@@ -1,4 +1,5 @@
-/*  Setup all 396 pins using 50 SR (Shift Registers.
+/*
+    Setup all 396 pins using 50 SR (Shift Registers.
     Find a way to make life easier by ma-
     -king certain functions and objects.
 */
