6ddd7364d96ce84c5e4d42efff748d6adcdeff6b
lesterrobotics <134779190+lesterrobotics@users.noreply.github.com>
2023-10-31 20:04:19 +0800

    Update abdulbukul.ino

--- testing/abdulbukul.ino
+++ testing/abdulbukul.ino
@@ -1,11 +1,27 @@
 /*
-    Setup all 396 pins using 50 SR (Shift 
-    Registers). Find a way to make life
-    easier by making certain functions 
-    and objects.
-*/
+ *  Setup all 396 pins using 50 SR (Shift 
+ *  Registers). Find a way to make life
+ *  easier by making certain functions 
+ *  and objects.
+ *
+ *  Idea 1: digitalWrite
+ *  Being able to write a certain
+ *  function similar to digitalWrite.
+ */
 
-class 74hc595
+class SR
 {
+    private:
 
+    byte dataPin, shiftPin, latchPin;
+    byte data = 0b00000000
+
+    public:
+
+    SR ( byte DP, byte SP, byte LP, int RDP_first, int RDP_last )
+    {
+        dataPin = DP;
+        shiftPin = SP;
+        latchPin = LP;
+        
 };
