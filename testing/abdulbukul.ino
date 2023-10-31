ab7847876eeb86dfc3accf357819c4ce4f15a642
lesterrobotics <134779190+lesterrobotics@users.noreply.github.com>
2023-10-31 19:09:11 +0800

    Update abdulbukul.ino

--- testing/abdulbukul.ino
+++ testing/abdulbukul.ino
@@ -1,6 +1,6 @@
 /*  Setup all 396 pins using 50 SR (Shift Registers.
-    Find a way to make life easier by making ce-
-    -rtain functions and objects.
+    Find a way to make life easier by ma-
+    -king certain functions and objects.
 */
 
 class 74hc595
