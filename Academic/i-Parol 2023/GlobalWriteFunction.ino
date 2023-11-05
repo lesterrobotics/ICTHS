822378544461332f5216067cfa6a74fffccd47e4
lesterrobotics <134779190+lesterrobotics@users.noreply.github.com>
2023-11-06 03:56:18 +0800

    Create GlobalWriteFunction.ino

---
+++ Academic/i-Parol 2023/GlobalWriteFunction.ino
@@ -0,0 +1,108 @@
+// Made by Lester Matthew Sollano
+
+// Class Constructor:
+// ShiftRegisterClass objectName ( dataPin, clockPin )
+// dataPin - DS Pin of 74HC595
+// clockPin - SHCP Pin of 74HC595
+
+// Class Functions:
+// write ( pin, state )
+
+// Global Functions:
+// globalWrite ( pin, state )
+
+
+#define print(x) Serial.println(x)
+#define latchPin 2
+
+int binStrToInt ( String binStr )
+{
+  return strtol ( binStr.c_str(), NULL, 2 );
+}
+
+String binIntToStr ( int binInt )
+{
+  return String( binInt, BIN );
+}
+
+class ShiftRegisterClass
+{
+  private:
+  int dataPin, clockPin, firstDataInt, secondDataInt;
+  String firstDataStr = "00000000", secondDataStr = "00000000", tempStr;
+
+  public:
+  ShiftRegisterClass ( int dataPin, int clockPin )
+  {
+    this->dataPin = dataPin;
+    this->clockPin = clockPin;
+  }
+
+  write ( int pin, int state )
+  {
+    if ( pin <= 8 ) {
+      tempStr = firstDataStr;
+      firstDataStr = "";
+      for ( int i = 0; i < 8; i++ )
+      {
+        if ( i+1 != pin )
+        {
+          firstDataStr = firstDataStr + tempStr[i];
+        } else
+        {
+          firstDataStr = firstDataStr + binIntToStr( state );
+        }
+      }
+      firstDataInt = binStrToInt ( firstDataStr );
+    } else
+    {
+      tempStr = secondDataStr;
+      secondDataStr = "";
+      for ( int i = 0; i < 8; i++ )
+      {
+        if ( i+9 != pin )
+        {
+          secondDataStr = secondDataStr + tempStr[i];
+        } else
+        {
+          secondDataStr = secondDataStr + binIntToStr( state );
+        }
+      }
+      secondDataInt = binStrToInt ( secondDataStr );
+    }
+    shiftOut ( dataPin, clockPin, LSBFIRST, secondDataInt );
+    shiftOut ( dataPin, clockPin, LSBFIRST, firstDataInt );
+    digitalWrite ( latchPin, HIGH );
+    digitalWrite ( latchPin, LOW );
+  }
+};
+
+ShiftRegisterClass SR[] = {ShiftRegisterClass( 4, 3 )};
+
+void globalWrite ( int pin, int state )
+{
+  int classIndex = (pin-1) / 16;
+  int outputPin = pin % 16;
+  SR[classIndex].write ( outputPin, state );
+}
+
+void allMode( int firstPin, int lastPin, int IO )
+{
+  int amtOfPins = lastPin - firstPin + 1;
+  for ( int counter = firstPin; counter <= lastPin; counter++ )
+  {
+    pinMode( counter, IO );
+  }
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+  allMode ( 0, 53, OUTPUT );
+}
+
+void loop()
+{
+  
+}
